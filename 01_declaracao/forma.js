8e3540f966a57ecd01ae5d8eccc543e693ec580f
Carlos Alberto <carlos.soares.3000@gmail.com>
2024-03-19 22:23:03 -0300

    classe forma

---
+++ 01_declaracao/forma.js
@@ -0,0 +1,19 @@
+    class Forma {
+
+        constructor() {
+
+            this.tipoDeForma = 'Forma Abstrata';
+        }
+
+        imprimeForma() {
+
+            // alert('Tipo de forma: ${this.tipoDEForma} - Área: ${this.calculaArea()}');
+            alert('Tipo de forma: ${this.tipoDeForma}');
+
+        }
+
+        calculaArea() {
+            throw new Error('O método calculaArea() deve ser implementado na subclasse.');
+
+        }
+    }
